6ede9c00aa21b273930c2e7cbfcf6ece37d08bf2
jcuush <jrcushing@sbcglobal.net>
2020-10-14 16:39:18 -0500

    added questions in index.js and provided readme layout in 'generateMarkdown.js'

--- Develop/index.js
+++ Develop/index.js
@@ -1,15 +1,82 @@
+// requires generateMarkdown js for const
+const generateMarkdown = require("./utils/generateMarkdown");
+var inquirer = require("inquirer")
+// fs is a Node standard library package for reading and writing files
+var fs = require("fs");
+
 // array of questions for user
 const questions = [
+    {
+        type: "input",
+        message: "What is the title of your project?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "What is the description of this project?",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "What are the installation instructions?",
+        name: "installation"
+    },
+    {
+        type: "list",
+        message: "What type of license is on this application?",
+        name: "license",
+        choices: [
+            "email",
+            "phone",
+            "telekinesis"
+        ]
+    },
+    {
+        type: "input",
+        message: "What is the project usage?",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "What are the contributions guidelines?",
+        name: "contributions"
+    },
+    {
+        type: "input",
+        message: "Provide the project tests",
+        name: "test"
+    },
+    {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "gitHubUser"
+    },
+    {
+        type: "input",
+        message: "What is your email?",
+        name: "email"
+    }
 
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    generateMarkdown(data);
 }
 
 // function to initialize program
 function init() {
+    // inquirer prompt will cause the questions to appear for user
+    inquirer
+    .prompt(questions);
+    // writes file 'README.md' , "utf8" encodes the raw buffer data in human-readable format
+    fs.writeFile("README.md", "utf8", function(error, data) {
 
+        if(error) {
+            return console.log(error);
+        }
+        console.log(data);
+    })
 }
 
 // function call to initialize program
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,46 @@
 // function to generate markdown for README
-function generateMarkdown(data) {
+function generateMarkdown(data, githubInfo) {
   return `# ${data.title}
+  ${data.badge}
+
+  ##Description
+
+  ${data.description}
+
+  ## Table of Contents
+
+  [Description](#Description)
+  [Installation](#Installation)
+  [Usage](#Usage)
+  [License](#License)
+  [Contributions](#Contributions)
+  [Test](#Test)
+  [Questions](#Questions)
+
+  ##Installation
+  
+  ${data.installation}
+
+  ##Usage
+
+  ${data.usage}
+
+  ##License
+
+  ${data.license}
+
+  ##Contributions
+
+  ${data.contributions}
+
+  ##Test
+
+  ${data.test}
+
+  ##Questions
+
+  ${data.gitHubUser}
+  ${data.email}
 
 `;
 }
